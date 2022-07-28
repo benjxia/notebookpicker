c939a7a9e8cb3b66f34016ec84652d74e3e70cd2
Ben <benjx32@gmail.com>
2022-07-27 21:29:47 -0700

    oracle cloud db connection established

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Data.SqlClient;
-using System.Data;
-using System;
+using Oracle.ManagedDataAccess.Client;
+using DotNetEnv;
 
 namespace notebookpicker.Controllers
 {
@@ -24,41 +23,49 @@ namespace notebookpicker.Controllers
         [HttpGet]
         public IEnumerable<Laptop> Get()
         {
-            string connectionString = "Data Source=.\\TEW_SQLEXPRESS;Initial Catalog=notebookpicker;Integrated Security=True";
-            string queryString = "SELECT * FROM NBINFO";
+            DotNetEnv.Env.Load();
+            OracleConnection oconn = new OracleConnection();
+            oconn.ConnectionString = Environment.GetEnvironmentVariable("CONNSTR");
+            oconn.Open();
+            OracleCommand cmd = new OracleCommand("SELECT * FROM NBINFO", oconn);
+            OracleDataReader rdr = cmd.ExecuteReader();
+            Console.WriteLine(rdr.FieldCount);
+            // string connectionString = "Data Source=.\\TEW_SQLEXPRESS;Initial Catalog=notebookpicker;Integrated Security=True";
+            // string queryString = "SELECT * FROM NBINFO";
             List<Laptop> output = new List<Laptop>();
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                SqlCommand command = new SqlCommand(queryString, connection);
-                connection.Open();
-                SqlDataReader reader = command.ExecuteReader();
-                
-                try
-                {
-                    while (reader.Read())
-                    {
-                        output.Add(new Laptop {
-                            ID = (long)reader["ID"],
-                            Brand = (string)reader["BRAND"],
-                            Name = (string)reader["NAME"],
-                            Release = (string)reader["RELEASE"],
-                            CPU = (string)reader["CPU"],
-                            GPU = (string)reader["GPU"],
-                            Memory = (int)reader["MEM"],
-                            StrType = (string)reader["STRTYPE"],
-                            StrSize = (int)reader["STRSIZE"],
-                            PanelType = (string)reader["PNTYPE"],
-                            Resolution = (string)reader["RESOLU"],
-                            AspRatio = (string)reader["ASPRATIO"],
-                            Size = (double)reader["SIZE"]
-                        });
-                    }
-                }
-                finally
-                {
-                    reader.Close();
-                }
-            }
+            // using (SqlConnection connection = new SqlConnection(connectionString))
+            // {
+            //     SqlCommand command = new SqlCommand(queryString, connection);
+            //     connection.Open();
+            //     SqlDataReader reader = command.ExecuteReader();
+            //     
+            //     try
+            //     {
+            //         while (reader.Read())
+            //         {
+            //             output.Add(new Laptop {
+            //                 ID = (long)reader["ID"],
+            //                 Brand = (string)reader["BRAND"],
+            //                 Name = (string)reader["NAME"],
+            //                 Release = (string)reader["RELEASE"],
+            //                 CPU = (string)reader["CPU"],
+            //                 GPU = (string)reader["GPU"],
+            //                 Memory = (int)reader["MEM"],
+            //                 StrType = (string)reader["STRTYPE"],
+            //                 StrSize = (int)reader["STRSIZE"],
+            //                 PanelType = (string)reader["PNTYPE"],
+            //                 Resolution = (string)reader["RESOLU"],
+            //                 AspRatio = (string)reader["ASPRATIO"],
+            //                 Size = (double)reader["SIZE"]
+            //                 // TODO: add weight?
+            //             });
+            //         }
+            //     }
+            //     finally
+            //     {
+            //         reader.Close();
+            //     }
+            // }
             return output;
         }
     }
