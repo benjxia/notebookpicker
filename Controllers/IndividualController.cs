15a7ea7287a7184a7aebe54f6299102aba43b103
Ben Xia <benjx32@gmail.com>
2022-08-18 14:30:51 -0700

    individual product pages (backend portion)

---
+++ Controllers/IndividualController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using notebookpicker.Data;
+using notebookpicker.Models;
+using System;
+namespace notebookpicker.Controllers;
+
+[ApiController]
+[Route("api/product")]
+public class IndividualController : ControllerBase
+{
+    private readonly ILogger<IndividualController> _logger;
+    private LaptopContext _context;
+
+    public IndividualController(ILogger<IndividualController> logger,
+        LaptopContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
+
+    [HttpGet]
+    public Laptop Get(string id)
+    {
+        return _context.Laptops.Include(l => l.Nbsellers).Include(l => l.Imgs)
+            .Where(x => x.Id == id).FirstOrDefault();
+
+    }
+    
+}
