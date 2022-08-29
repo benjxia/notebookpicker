2e60aa31a70b0fd64d9ff86567cd1c5e935851b6
Ben Xia <benjx32@gmail.com>
2022-08-29 00:38:19 -0700

    navbar 2.0

--- ClientApp/src/components/Layout.js
+++ ClientApp/src/components/Layout.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Container } from 'reactstrap';
-import { NavMenu } from './NavMenu';
+import { NavBar } from './NavBar'
 
 export class Layout extends Component {
   static displayName = Layout.name;
@@ -8,7 +8,7 @@ export class Layout extends Component {
   render() {
     return (
       <div>
-        <NavMenu />
+        <NavBar />
         <Container>
           {this.props.children}
         </Container>
---
+++ ClientApp/src/components/NavBar.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import logo from '../assets/branding/nbpicker.png';
+import { Link } from 'react-router-dom';
+import { Navbar, NavbarBrand, NavItem, NavLink } from 'reactstrap';
+import "./NavBar.css";
+
+export class NavBar extends Component {
+  static displayName = NavBar.name;
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <header>
+        <Navbar className="NavigationBar" container light>
+          <NavbarBrand href="/"> <img alt="logo" src={logo} style={{ height: 50, width: 50 }} /></NavbarBrand>
+          <NavbarBrand href="/" className="button"> NotebookPicker </NavbarBrand>
+          <NavItem>
+            <NavLink tag={Link} className="button" to="/products">
+              Browse Products
+            </NavLink>
+          </NavItem>
+        </Navbar>
+      </header>
+    );
+  }
+}
