0ad6d70d43f49a013fe068416cbed0f65ab906d8
Ben Xia <benjx32@gmail.com>
2022-09-15 23:52:56 -0700

    sorting frontend

--- ClientApp/src/components/ProdComp/ProductTable.js
+++ ClientApp/src/components/ProdComp/ProductTable.js
@@ -1,69 +1,112 @@
+// import * as React from 'react';
 import React, { Component } from 'react';
-
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TablePagination from '@mui/material/TablePagination';
-import TableRow from '@mui/material/TableRow';
-import TableSortLabel from '@mui/material/TableSortLabel';
+import Box from '@mui/material/Box';
+import { DataGrid } from '@mui/x-data-grid';
+import "../styles/Productsv2.css"
 
 export class ProductTable extends Component {
     constructor(props) {
         super(props);
+        this.state = {};
     }
-
-    createData(number, item, qty, price) {
-        return { number, item, qty, price };
-    }
-
+    
     render() {
-        const rows = this.props.products;
+        var columns = [
+            {
+                field: 'name',
+                headerName: 'Name',
+                width: 300,
+                headerClassName: 'product-table-header',
+                renderCell: (params) => {
+                    return <div><img src = {params.row.imgP} alt = "laptop" height = {50} width = {50}/>{params.value}</div>
+                }
+            },
+            {
+                field: 'cpu',
+                headerName: 'CPU',
+                width: 150,
+                headerClassName: 'product-table-header'
+            },
+            {
+                field: 'gpu',
+                headerName: 'GPU',
+                width: 150,
+                headerClassName: 'product-table-header'
+            },
+            {
+                field: 'memory',
+                headerName: 'Memory (GB)',
+                width: 80,
+                headerClassName: 'product-table-header',
+                type: 'number',
+            },
+            {
+                field: 'strType',
+                headerName: 'Storage Type',
+                width: 120,
+                headerClassName: 'product-table-header'
+            },
+            {
+                field: 'strSize',
+                headerName: 'Storage (GB)',
+                width: 80,
+                headerClassName: 'product-table-header',
+                type: 'number',
+            },
+            {
+                field: 'panelType',
+                headerName: 'Panel',
+                width: 80,
+                headerClassName: 'product-table-header'
+            },
+            {
+                field: 'resolution',
+                headerName: 'Resolution',
+                width: 100,
+                headerClassName: 'product-table-header'
+            },
+            {
+                field: 'size',
+                headerName: 'Size (in.)',
+                width: 60,
+                headerClassName: 'product-table-header',
+                type: 'number',
+                renderCell: (params) => {
+                    return <div>{params.value.toFixed(1)}</div>
+                }
+            },
+            {
+                field: 'weight',
+                headerName: 'Weight (lbs)',
+                width: 75,
+                headerClassName: 'product-table-header',
+                type: 'number',
+            },
+            {
+                headerName: 'Price',
+                width: 80,
+                headerClassName: 'product-table-header',
+                type: 'number',
+                valueGetter: (params) => {
+                    return params.row.nbsellers.length === 0 ? "N/A": "$" + params.row.nbsellers.reduce((min, p) => p.price < min ? p.price : min, params.row.nbsellers[0].price).toFixed(2);
+                }
+            }
+        ];
+
+        var rows = this.props.products
         return (
-          <div>
-            {this.props.products.length}
-            <TableContainer>
-                <Table aria-label="simple table">
-                <TableHead>
-                    <TableRow>
-                    <TableCell></TableCell>
-                    <TableCell align = "left">Name</TableCell>
-                    <TableCell align = "right">CPU</TableCell>
-                    <TableCell align = "right">GPU</TableCell>
-                    <TableCell align = "right">Memory (GB)</TableCell>
-                    <TableCell align = "right">Storage Type</TableCell>
-                    <TableCell align = "right">Storage Capacity (GB)</TableCell>
-                    <TableCell align = "right">Screen Type</TableCell>
-                    <TableCell align = "right">Screen Resolution</TableCell>
-                    <TableCell align = "right">Display Size (Inches)</TableCell>
-                    <TableCell align = "right">Weight (lbs)</TableCell>
-                    <TableCell align = "right">Price (USD)</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows.map((row) => (
-                    <TableRow key={row.name}>
-                        <TableCell><img src = {row.imgP} alt = "laptop" height = {50} width = {50}/></TableCell>
-                        <TableCell component="TableCell" scope="row" align = "left">
-                        {row.name}
-                        </TableCell>
-                        <TableCell align="right">{row.cpu}</TableCell>
-                        <TableCell align="right">{row.gpu}</TableCell>
-                        <TableCell align="right">{row.memory}</TableCell>
-                        <TableCell align="right">{row.strType}</TableCell>
-                        <TableCell align="right">{row.strSize}</TableCell>
-                        <TableCell align="right">{row.panelType}</TableCell>
-                        <TableCell align="right">{row.resolution}</TableCell>
-                        <TableCell align="right">{row.size.toFixed(1)}</TableCell>
-                        <TableCell align="right">{row.weight}</TableCell>
-                        <TableCell align="right">{row.nbsellers.length === 0 ? "N/A" : "$" + row.nbsellers.reduce((min, p) => p.price < min ? p.price : min, row.nbsellers[0].price).toFixed(2)}</TableCell>
-                    </TableRow>
-                    ))}
-                </TableBody>
-                </Table>
-            </TableContainer>
-          </div>  
+            <div style = {{height: 1200, width: '100%'}}>
+                <DataGrid
+                    rows={rows}
+                    columns={columns}
+                    pageSize={100}
+                    rowsPerPageOptions={[100]}
+                    disableSelectionOnClick
+                    disableColumnFilter
+                    disableColumnSelector
+                    experimentalFeatures={{ newEditingApi: true }}
+                />
+            </div>
         );
     }
 }
